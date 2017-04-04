9f3a8b155dad000afae9819057c75847482fb369
alphonseevalle <alphonseevalle@gmail.com>
2017-04-05 00:11:42 +0800

    Merged movement and A* search into pathplanner.cpp

--- pathplanner.cpp
+++ pathplanner.cpp
@@ -7,8 +7,272 @@
 #include <cmath>
 #include <cstdlib>
 #include <queue>
+#include <stack>
 
 using namespace std;
+
+//Function Prototypes
+double LiftOff(double x_pos, double y_pos, double z_pos, double x_vel, double y_vel, double z_vel, double x_acc, double y_acc, double z_acc, double heading, double ang_vel, ofstream& outfile)	{
+	double z_pos_prev = z_pos;
+	double z_vel_prev = z_vel;
+	double z_acc_prev = 0.0;
+	int z_count = 0;
+
+	double z_pos_next = 0.0;
+	double z_vel_next = 0.0;
+	double z_acc_next = z_acc;
+
+	bool phase_1 = true;
+	bool phase_2 = true;
+	bool phase_3 = true;
+
+	outfile << x_pos << " " << y_pos << " " << z_pos_prev << " " << x_vel << " " << y_vel << " " << z_vel_prev << " " << x_acc << " " << y_acc << " " << z_acc_prev << " " << heading << " " << ang_vel << endl;
+
+	while (phase_1)
+	{
+		z_vel_next = 0.5*(z_acc_prev + z_acc_next)*0.05 + z_vel_prev;
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+
+		z_pos_prev = z_pos_next;
+		z_vel_prev = z_vel_next;
+		z_acc_prev = z_acc_next;
+		z_count++;
+
+		if (z_vel_next > 0.35)
+		z_acc_next = 0.0;
+
+		if (z_acc_prev < 0.01)
+		phase_1 = false;
+	}
+
+	z_acc_next = 0.0;
+	double z_pos_covered = z_pos_prev;
+	while (phase_2)
+	{
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+		z_pos_prev = z_pos_next;
+
+		if (z_pos_next > 1.0 - z_pos_covered)
+		phase_2 = false;
+	}
+
+	z_acc_next = -0.5;
+	for (int i = 0; i < z_count; i++)
+	{
+		z_vel_next = 0.5*(z_acc_prev + z_acc_next)*0.05 + z_vel_prev;
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+
+		z_pos_prev = z_pos_next;
+		z_vel_prev = z_vel_next;
+		z_acc_prev = z_acc_next;
+
+		if (i == z_count - 2)
+		z_acc_next = 0.0;
+
+	}
+
+	z_acc_next = 0.0;
+	//outfile << x_pos << " " << y_pos << " " << z_p << " " << x_vel << " " << y_vel << " " << z_v << " " << x_acc << " " << y_acc << " " << z_a << " " << heading << " " << ang_vel << endl;
+
+	return z_pos_next;
+}
+
+void Landing(double x_pos, double y_pos, double z_pos, double x_vel, double y_vel, double z_vel, double x_acc, double y_acc, double z_acc, double heading, double ang_vel, ofstream& outfile)	{
+	double z_pos_prev = z_pos;
+	double z_vel_prev = z_vel;
+	double z_acc_prev = 0.0;
+	int z_count = 0;
+
+	double z_pos_next = 0.0;
+	double z_vel_next = 0.0;
+	double z_acc_next = z_acc;
+
+	bool phase_1 = true;
+	bool phase_2 = true;
+	bool phase_3 = true;
+
+	outfile << x_pos << " " << y_pos << " " << z_pos_prev << " " << x_vel << " " << y_vel << " " << z_vel_prev << " " << x_acc << " " << y_acc << " " << z_acc_prev << " " << heading << " " << ang_vel << endl;
+
+	while (phase_1)
+	{
+		z_vel_next = 0.5*(z_acc_prev + z_acc_next)*0.05 + z_vel_prev;
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+
+		z_pos_prev = z_pos_next;
+		z_vel_prev = z_vel_next;
+		z_acc_prev = z_acc_next;
+		z_count++;
+
+		if (z_vel_next < -0.35)
+		z_acc_next = 0.0;
+
+		if (z_acc_prev > -0.01)
+		phase_1 = false;
+	}
+
+	z_acc_next = 0.0;
+	double z_pos_covered = 1.0 - z_pos_prev;
+	while (phase_2)
+	{
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+		z_pos_prev = z_pos_next;
+
+		if (z_pos_next < z_pos_covered)
+		phase_2 = false;
+	}
+
+	z_acc_next = 0.5;
+	/*
+	while (phase_3)
+	{
+		z_vel_next = 0.5*(z_acc_prev + z_acc_next)*0.05 + z_vel_prev;
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+
+		z_pos_prev = z_pos_next;
+		z_vel_prev = z_vel_next;
+		z_acc_prev = z_acc_next;
+		
+	}*/
+	for (int i = 0; i < z_count; i++)
+	{
+		z_vel_next = 0.5*(z_acc_prev + z_acc_next)*0.05 + z_vel_prev;
+		z_pos_next = 0.5*(z_vel_prev + z_vel_next)*0.05 + z_pos_prev;
+
+		outfile << x_pos << " " << y_pos << " " << z_pos_next << " " << x_vel << " " << y_vel << " " << z_vel_next << " " << x_acc << " " << y_acc << " " << z_acc_next << " " << heading << " " << ang_vel << endl;
+
+		z_pos_prev = z_pos_next;
+		z_vel_prev = z_vel_next;
+		z_acc_prev = z_acc_next;
+
+		if (i == z_count - 2)
+		z_acc_next = 0.0;
+
+	}
+
+	z_acc_next = 0.0;
+	//outfile << x_pos << " " << y_pos << " " << z_p << " " << x_vel << " " << y_vel << " " << z_v << " " << x_acc << " " << y_acc << " " << z_a << " " << heading << " " << ang_vel << endl;
+
+}
+
+void Move(double& x_pos, double y_pos, double z_pos, double x_vel, double y_vel, double z_vel, double x_acc, double y_acc, double z_acc, double heading, double ang_vel, ofstream& outfile)		{
+	//x-direction
+	double x_pos_prev = x_pos;
+	double x_vel_prev = 0.0;
+	double x_acc_prev = 0.0;
+	int x_count = 0;
+
+	double x_pos_next = 0.0;
+	double x_vel_next = 0.0;
+	double x_acc_next = x_acc;
+	
+	//y-direction
+	double y_pos_prev = y_pos;
+	double y_vel_prev = 0.0;
+	double y_acc_prev = 0.0;
+	int y_count = 0;
+
+	double y_pos_next = 0.0;
+	double y_vel_next = 0.0;
+	double y_acc_next = y_acc;
+
+	
+	
+	
+	bool phase_1 = true;
+	bool phase_2 = true;
+	bool phase_3 = true;
+
+	outfile << x_pos_prev << " " << y_pos << " " << z_pos << " " << x_vel_prev << " " << y_vel << " " << z_vel << " " << x_acc_prev << " " << y_acc << " " << z_acc << " " << heading << " " << ang_vel << endl;
+
+	while (phase_1)
+	{
+		//x-direction
+		x_vel_next = 0.5*(x_acc_prev + x_acc_next)*0.05 + x_vel_prev;
+		x_pos_next = 0.5*(x_vel_prev + x_vel_next)*0.05 + x_pos_prev;
+		
+		//y-direction
+		y_vel_next = 0.5*(y_acc_prev + y_acc_next)*0.05 + y_vel_prev;
+		y_pos_next = 0.5*(y_vel_prev + y_vel_next)*0.05 + y_pos_prev;
+		
+		outfile << x_pos_next << " " << y_pos << " " << z_pos << " " << x_vel_next << " " << y_vel << " " << z_vel << " " << x_acc_next << " " << y_acc << " " << z_acc << " " << heading << " " << ang_vel << endl;
+
+		x_pos_prev = x_pos_next;
+		x_vel_prev = x_vel_next;
+		x_acc_prev = x_acc_next;
+		x_count++;
+		
+		y_pos_prev = y_pos_next;
+		y_vel_prev = y_vel_next;
+		y_acc_prev = y_acc_next;
+		
+		if (x_vel_next > 0.35)
+		x_acc_next = 0.0;
+		y_acc_next = 0.0;
+
+		if (x_acc_prev < 0.01)
+		phase_1 = false;
+	}
+
+	x_acc_next = 0.0;
+	double x_pos_covered = x_pos_prev-x_pos;
+	
+	while (phase_2)
+	{
+		
+		x_pos_next = 0.5*(x_vel_prev + x_vel_next)*0.05 + x_pos_prev;
+		y_pos_next = 0.5*(y_vel_prev + y_vel_next)*0.05 + y_pos_prev;
+
+		outfile << x_pos_next << " " << y_pos << " " << z_pos << " " << x_vel_next << " " << y_vel << " " << z_vel << " " << x_acc_next << " " << y_acc << " " << z_acc << " " << heading << " " << ang_vel << endl;
+		
+		x_pos_prev = x_pos_next;
+		y_pos_prev = y_pos_next;
+		
+		if (x_pos_next > x_pos + 1.0 - fabs(x_pos_covered))
+		phase_2 = false;
+	}
+
+	x_acc_next = -0.5;
+	y_acc_next = -0.5;
+	for (int i = 0; i < x_count; i++)
+	{
+		x_vel_next = 0.5*(x_acc_prev + x_acc_next)*0.05 + x_vel_prev;
+		x_pos_next = 0.5*(x_vel_prev + x_vel_next)*0.05 + x_pos_prev;
+		y_vel_next = 0.5*(y_acc_prev + y_acc_next)*0.05 + y_vel_prev;
+		y_pos_next = 0.5*(y_vel_prev + y_vel_next)*0.05 + y_pos_prev;
+
+		outfile << x_pos_next << " " << y_pos << " " << z_pos << " " << x_vel_next << " " << y_vel << " " << z_vel << " " << x_acc_next << " " << y_acc << " " << z_acc << " " << heading << " " << ang_vel << endl;
+
+		x_pos_prev = x_pos_next;
+		x_vel_prev = x_vel_next;
+		x_acc_prev = x_acc_next;
+
+		if (i == x_count - 2)	{
+			x_acc_next = 0.0;
+			y_acc_next = 0.0;
+		}
+
+	}
+
+	x_acc_next = 0.0;
+	x_pos = x_pos_next;
+	y_acc_next = 0.0;
+	y_pos = y_pos_next;
+	//y_pos = y_pos_next;
+	//outfile << x_pos << " " << y_pos << " " << z_p << " " << x_vel << " " << y_vel << " " << z_v << " " << x_acc << " " << y_acc << " " << z_a << " " << heading << " " << ang_vel << endl;
+
+}
 
 struct cell	{
 	//Coordinates
@@ -26,26 +290,9 @@ struct cell	{
 
 	//Parent celll
 	cell* parentPtr = NULL;
-	/*	
-		Label each cell as XXYY
-	*/
+
+	//Label each cell as XXYY
 	int getID()	{
-		/*
-		int sum = fabs(x*1000) + fabs(y*10);
-		if(x < 0)	{
-			sum = sum*10 + 1;
-		}
-		else	{
-			sum = sum*10;
-		}
-		if(y < 0)	{
-			sum = sum*10 + 1;
-		}
-		else	{
-			sum = sum*10;
-		}
-		return sum;
-		*/
 		id = (int) (x*1000) + (int) (y*10);
 		return id;
 	}
@@ -74,17 +321,36 @@ bool checkGoal(cell curr, cell end)	{
 	
 }
 
+stack<cell> path(cell end, cell start)	{
+	cell current = end;
+	cell *tmp;
+	tmp=&current;
+	stack<cell> visitedPath;
 
-queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
+	while (current.parentPtr != NULL)	{
+		//cout << "ParentCell is not NULL " << current.parentPtr  << " " << &current << endl;
+		visitedPath.push(current);
+		tmp = current.parentPtr;
+		current = *tmp;
+	}
+	//visitedPath.push(start);
+	return visitedPath;
+}
+
+
+stack<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 	//Initialize start and end cells 
 	start.g = 0;
 	start.id = start.getID();
 	end.id = end.getID();
 	start.h = start.getH(end);
+	stack<cell> empty_stack;
 	
 	//Create an open and closed list
 	vector<cell> open_list;
+	open_list.clear();
 	vector<cell> closed_list;
+	closed_list.clear();
 	queue<cell> path_q;
 	bool inOpen = false;
 	bool inClosed = false;
@@ -92,9 +358,9 @@ queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 	int tmp = 0;
 	
 	//Add start to open list
-	cell curr;
 	open_list.push_back(start);
-	curr = start;
+	cell tempo=start;
+	
 	
 	/*
 		For checking adjacent cells	
@@ -107,7 +373,8 @@ queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 	int dir_y[8] = {0,-1,-1,-1,0,1,1,1};
 	
 	while(!open_list.empty())	{
-
+		cell* curr= new cell;
+		//cout << "------------------------------" << endl;
 		//Check if current cell is the end
 		/* 		if(checkGoal(curr,end))	{
 			cout << "Goal reached" << endl;
@@ -116,6 +383,7 @@ queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 		
 		//Search for cell with smallest F cost
 		cell temp = open_list[0];
+		tmp = 0;
 		for(int idx = 0; idx < open_list.size(); idx++)	{
 			if(open_list[idx].getF() < temp.getF())	{
 				temp = open_list[idx];
@@ -123,47 +391,49 @@ queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 			}
 		}
 		
-		curr = temp;
-		cout << curr.x << " " << curr.y << " " << curr.id << endl;
-		if(checkGoal(curr,end))	{
+		(*curr) = temp;
+		//cout << (*curr).x << " " << (*curr).y << " " << (*curr).id << " " << &curr  << " " << (*curr).parentPtr << endl;
+		if(checkGoal((*curr),end))	{
 			cout << "Goal reached" << endl;
-			return path_q;
+			return path((*curr), start);
 		} 
 		
 		//Remove chosen cell from open_list and add it to closed_list
-		closed_list.push_back(curr);
+		closed_list.push_back((*curr));
 		open_list.erase(open_list.begin()+tmp);
-		
+		//cout << open_list.size() << " " << closed_list.size() << endl;
 		//Check each neighbor of the current cell
 		for(int k = 0; k < 8; k++)	{
 			
-			cell new_cell;
-			new_cell.x = curr.x + dir_x[k]*0.5;
-			new_cell.y = curr.y + dir_y[k]*0.5;
-			new_cell.getID();
-			new_cell.getH(end);
-			new_cell.getF();
-			//cout << new_cell.x << " " << new_cell.y << " " << new_cell.id << endl;
+			cell* new_cell= new cell;
+			(*new_cell).x = (*curr).x + dir_x[k]*0.5;
+			(*new_cell).y = (*curr).y + dir_y[k]*0.5;
+			(*new_cell).getID();
+			(*new_cell).getH(end);
+			(*new_cell).getF();
+			//cout << (*new_cell).x << " " << (*new_cell).y << " " << (*new_cell).id << endl;
 			
 			//If the cells is a wall, mark them as obstacles
-			if (new_cell.x <-2.45 || new_cell.x > 2.45 || new_cell.y <-2.45 || new_cell.y > 2.45)	{
-				new_cell.obs = true;
-				closed_list.push_back(new_cell);
+			if ((*new_cell).x <-2.45 || (*new_cell).x > 2.45 || (*new_cell).y <-2.45 || (*new_cell).y > 2.45)	{
+				(*new_cell).obs = true;
+				closed_list.push_back((*new_cell));
 				//cout << "Wall" << endl;
 			}		
 			
 			//Check if cell is in the obs vector
 			for(int idx = 0; idx < obs.size(); idx++)	{
-				if(new_cell.id == obs[idx])	{
-					new_cell.obs = true;
-					closed_list.push_back(new_cell);
+				if((*new_cell).id == obs[idx])	{
+					(*new_cell).obs = true;
+					closed_list.push_back((*new_cell));
+					//cout << "cell is obstacle" << endl;
 				}
 			} 
 			
 			//Check if cell is in closed_list
 			for(int idx = 0; idx < closed_list.size(); idx++)	{
-				if(new_cell.id == closed_list[idx].id)	{
+				if((*new_cell).id == closed_list[idx].id)	{
 					inClosed = true;
+					//cout << "cell is in closed list" << endl;
 					break;
 				}
 			}
@@ -177,48 +447,52 @@ queue<cell> pathSearch(cell start, cell end, vector<int> obs)	{
 			//Get tentative g score
 			int tmp_g;
 			if(k%2 == 0)	{
-				tmp_g = curr.g + 10;
+				tmp_g = (*curr).g + 10;
 			}
 			else	{
-				tmp_g = curr.g + 14;
+				tmp_g = (*curr).g + 14;
 			}
 			
 			//Check if cell is in open_list
 			for(int idx = 0; idx < open_list.size(); idx++)	{
-				if(new_cell.id == open_list[idx].id)	{
+				if((*new_cell).id == open_list[idx].id)	{
 					inOpen = true;
 					o_idx = idx;
+					//cout << "cell is in open list" << endl;
 					break;
 				}
 			}
 			//If not in open_list, add to list
 			if(!inOpen)	{
-				new_cell.parentPtr = &curr;
-				new_cell.g = tmp_g;
-				new_cell.getH(end);
-				new_cell.getF();
-				open_list.push_back(new_cell);
+				(*new_cell).parentPtr = curr;
+				(*new_cell).g = tmp_g;
+				(*new_cell).getH(end);
+				(*new_cell).getF();
+				open_list.push_back((*new_cell));
+				//cout << "cell is not in open list" << endl;
 			}
 			//If cell is in open list, check if F score is lower than current path
 			else{
 				inOpen = false;
 				//If F score is larger, ignore cell
 				if(tmp_g >= open_list[o_idx].g)	{
+					//cout << "cost is higher" << endl;
 					continue;
 				}
 				//Else, update score of cell in the open list
 				else{
+					//cout << "updating scores" << endl;
 					open_list[o_idx].g = tmp_g;
 					int f = open_list[o_idx].getF();
-					open_list[o_idx].parentPtr = &curr;
+					open_list[o_idx].parentPtr = curr;
 					//curr = open_list[o_idx];
-					path_q.push(curr);
+					//path_q.push(curr);
 				}
 			}
 		}
 	}
 	
-	return path_q;
+	return empty_stack;
 	
 }
 
@@ -245,21 +519,66 @@ int main()	{
 			obs_y = y + odir_y[idx]*0.5;
 			obs.push_back((int) (obs_x*1000) + (int) (obs_y*10));
 		}
-	
+		
 	}
 	
 	for(int idx = 0; idx < obs.size(); idx++)	{
 		cout << obs[idx] << endl;
 	}
+	
 	//A* Algorithm
 	cell start, end;
 	start.x = -1.5;
 	start.y = 1.5;
 	end.x = 2.0;
-	end.y = 0.0;
-	queue<cell> visited_path;
+	end.y = 0;
+	
+	start.x = -1.5;
+	start.y = -1.5;
+	end.x = 2.0;
+	end.y = 0;
+	
+	start.x = 2.0;
+	start.y = 0;
+	end.x = -1.5;
+	end.y = -1.5;
+	
+	stack<cell> visited_path;
 	cout << "starting path search" << endl;
 	visited_path = pathSearch(start,end,obs);
 	cout << "done searching" << endl;
 	
+	while(!visited_path.empty())	{
+		cell temp;
+		
+		temp = visited_path.top();
+		cout << temp.x << " " << temp.y << endl;
+		visited_path.pop();
+		
+	}
+	//Generate drone path
+	ofstream outfile;
+	outfile.open("path.txt");
+	cout.setf(ios::showpoint);
+	cout.precision(3);
+	int z_pos;
+	//Take Off
+	z_pos = LiftOff(start.x, start.y, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, outfile);
+	while(!visited_path.empty())	{
+		cell temp;
+		
+		temp = visited_path.top();
+		cout << temp.x << " " << temp.y << endl;
+		Move(temp.x, temp.y, z_pos, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, outfile);
+		visited_path.pop();
+	}
+	Landing(end.x, end.y, z_pos, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5, 0.0, 0.0, outfile);
 }
+
+/*
+	Movement Handling Functions
+
+
+
+*/
+
